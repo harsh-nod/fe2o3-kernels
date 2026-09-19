81dfb69ce673d945053fb90e0450897f9f9ea15a
harsh-nod <3144552+harsh-nod@users.noreply.github.com>
2026-09-18 17:54:02 -0700

    Bind first-fill tutorial to its complete published Rust source
    Signed-off-by: harsh-nod <3144552+harsh-nod@users.noreply.github.com>

--- examples/fill_kernel.rs
+++ examples/fill_kernel.rs
@@ -4,7 +4,6 @@ use fe2o3_device::{DisjointSlice, kernel, thread};
 
 #[kernel(
     typed,
-    namespace = "3f959016b22cc527afdf32bf2ed9b043947c2147348f1ab939488dab760220e5",
     launch(required = [64, 1, 1], max = [64, 1, 1]),
 )]
 pub fn fill(mut out: DisjointSlice<f32>) {
